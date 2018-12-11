30bc816bb24ea04ab5eaf9a570a38e05818dbd8c
Mikita <nikcheb98@gmail.com>
2018-12-11 23:08:06 +0100

    formatting

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,32 +1,34 @@
-import numpy as np 
+import numpy as np
 import random
 import math
-from time import sleep
-from utils import read_matrix, greedy_solution, calc_tour_length     
+import time
+from utils import read_matrix, greedy_solution, calc_tour_length
+
 
 def random_tour(n):
-    return random.sample(range(1,n), n-1)      
+    return random.sample(range(1, n), n - 1)
 
 
-def swap_random_elements(state): #better
+def swap_random_elements(state):  # better
     n = len(state)
     newstate = list(state)
-    i = random.randint(0, n-1)
-    j = random.randint(0, n-1)
+    i = random.randint(0, n - 1)
+    j = random.randint(0, n - 1)
     if i > j:
         newstate[j], newstate[i] = newstate[i], newstate[j]
-    else: 
+    else:
         newstate[i], newstate[j] = newstate[j], newstate[i]
     return newstate
+
 
 def swap_random_neighbors(state):
     n = len(state)
     newstate = list(state)
-    i = random.randint(0, n-1)
-    j = i+1 if i != n-1 else -1
+    i = random.randint(0, n - 1)
+    j = i + 1 if i != n - 1 else -1
     if i > j:
         newstate[j], newstate[i] = newstate[i], newstate[j]
-    else: 
+    else:
         newstate[i], newstate[j] = newstate[j], newstate[i]
     return newstate
 
@@ -46,12 +48,14 @@ def invert(state):
 
 
 def transition_probability(delta_energy, T):
-    return math.exp(-delta_energy/T)
+    return math.exp(-delta_energy / T)
+
 
 def make_transition(p):
-    return True if random.uniform(0,1) <= p else False 
+    return True if random.uniform(0, 1) <= p else False
 
-def SimulatedAnnealing(cities, init_T, end_T, cooling_factor,init_state=None):
+
+def SimulatedAnnealing(cities, init_T, end_T, cooling_factor, init_state=None):
     n = len(cities)
     state = greedy_solution(cities) if init_state is None else init_state
     current_energy = calc_tour_length(state, cities)
@@ -59,10 +63,11 @@ def SimulatedAnnealing(cities, init_T, end_T, cooling_factor,init_state=None):
     best_state_energy = current_energy
     
     T = init_T
+    for _ in range(1, 100000):
     
     for _ in range(1,100000000):
         candidat_state = swap_random_elements(state)
-        candidant_energy  = calc_tour_length(candidat_state, cities)
+        candidant_energy = calc_tour_length(candidat_state, cities)
 
         if(candidant_energy < current_energy):
             current_energy = candidant_energy
